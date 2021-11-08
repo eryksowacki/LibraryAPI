7d1aaa26f073510707a2c5c6be60a93b0c288f8c
Eryk Sowacki <eryk.sowacki@op.pl>
2021-11-08 14:17:53 +0100

    .

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -42,7 +42,6 @@ namespace bibliotekaAPI.Controllers
         }
 
         // PUT: api/Books/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutBook(long id, Book book)
         {
@@ -73,7 +72,6 @@ namespace bibliotekaAPI.Controllers
         }
 
         // POST: api/Books
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<Book>> PostBook(Book book)
         {
--- Controllers/ShelvesController.cs
+++ Controllers/ShelvesController.cs
@@ -44,7 +44,6 @@ namespace bibliotekaAPI.Controllers
         }
 
         // PUT: api/Shelves/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutShelf(long id, Shelf shelf)
         {
@@ -75,7 +74,6 @@ namespace bibliotekaAPI.Controllers
         }
 
         // POST: api/Shelves
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<Shelf>> PostShelf(Shelf shelf)
         {
